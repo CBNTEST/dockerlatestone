3c7805d7d950edb3b266ff725d9db0ef6c4a6c73
lenovo <chandrababuguntur@gmail.com>
2022-06-25 21:19:55 +0530

    firstcoomit

---
+++ javatesting.java
@@ -0,0 +1,9 @@
+public class javatesting {
+    public static void main(String[] args)  //static method
+    {
+        System.out.println("Static method");
+        System.out.println("Static method");
+        System.out.println("Static method");
+    }
+
+}
